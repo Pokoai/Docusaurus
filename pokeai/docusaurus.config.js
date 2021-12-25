ce7dd5ce5455521c1a42c22dde155faced2480d7
pokoai <pokeusr@outlook.com>
2021-12-26 05:52:20 +0800

    增加留言板功能

--- pokeai/docusaurus.config.js
+++ pokeai/docusaurus.config.js
@@ -100,7 +100,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           {to: 'blog', label: '博客', position: 'right'},
           {to: 'about', label: '关于', position: 'right'},
           {
-            href: 'https://arctee.cn/guestbook',
+            href: 'https://waline-pokoai.vercel.app/',
+            // to: 'discussion',
             label: '留言',
             position: 'right',
           },
